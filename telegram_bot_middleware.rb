01130ab8c2df0806761ebb8b52bff34b75758795
Mirko Mignini <mirko.mignini@gmail.com>
2015-11-05 17:56:00 +0100

    Added a lot of comments

--- telegram_bot_middleware.rb
+++ telegram_bot_middleware.rb
@@ -13,56 +13,77 @@ class TelegramBotMiddleware
                                 force_retry: true
   
   def initialize(app, &block)
+    # save the app var
     @app = app
     
+    # create the config and populate passing do the block function
     @config = OpenStruct.new
     yield(@config) if block_given?
 
-
+    # setup webhook
     if @config.webhook.nil?
       @config.host = "#{@config.host}/" unless @config.host.end_with?('/')
       @config.webhook = "#{@config.host}#{@config.token}"
     end
     
+    # setup telegram messages input
     case @config.get_updates
       
+      # setup polling
       when :polling
+        # clear the webhook in case was set in the past
         send_to_bot('setWebhook', {url: ''})
         
-        Thread.new do
-          @offset = 0
-          sleep 5
-          loop do            
-            response = send_to_bot('getUpdates', {offset: @offset})
-            response.to_hash['result'].each do |data|
-              update = OpenStruct.new(data)
-              @offset = (update.update_id + 1) if update.update_id + 1 > @offset
-              HTTP.post @config.webhook, json: update.to_h_nested
-            end
-          end
-          
-        end
+        # setup a thread with get_updates function
+        start_get_updates_thread
       
+      # setup webhook
       when :webhook
         send_to_bot('setWebhook', {url: @config.webhook})
       
+      # in this case get_updates is a non valid value
       else
         raise ArgumentError.new('Config error: get_updates must be :webhook or :polling.')
     end
   end
+  
+  def start_get_updates_thread
+    # start a new thread
+    Thread.new do
+      # the initial offset is always 0
+      @offset = 0      
+      # wait 5 seconds to don't risk to post message too early when the app is not still up
+      sleep 5
+      # loop forever
+      loop do
+        # call the getUpdates telegram function
+        response = send_to_bot('getUpdates', {offset: @offset})
+        # enumerate the results
+        response.to_hash['result'].each do |data|
+          # create an update message from the post data
+          update = OpenStruct.new(data)
+          # store the last offset +1 but ensure that is not lower than the already stored
+          @offset = (update.update_id + 1) if update.update_id + 1 > @offset
+          # simulate a post to itself
+          HTTP.post @config.webhook, json: update.to_h_nested
+        end
+      end
+    end
+  end
 
   def call(env)
-    #retrieve the request object
+    # retrieve the request object
     req = Rack::Request.new(env)
     
-    #if the request is a post to bot webhhok
+    # if the request is a post to bot webhhok
     if req.post? and req.path == "/#{@config.token}"
       
-      #build an openstruct based on post params
-      req.body.rewind  # in case someone already read it
+      # in case someone already read it
+      req.body.rewind
+      # build an openstruct based on post params
       params = OpenStruct.from_json(req.body.read)
       
-      #build path based on message
+      # build path based on message
       # - get only message part of post params
       # - remove empty chars from beginning or end (strip)
       # - replace spaces with /
@@ -71,24 +92,20 @@ class TelegramBotMiddleware
       # - add first / if not present
       path = "/#{path}" unless path.start_with?('/')
       
-      #build the querystring using message but nested
+      # build the querystring using message but nested
       query_string = Rack::Utils.build_nested_query(params.message.to_h_nested)
       
-      #transform the POST in GET
+      # transform the POST in GET
       env['PATH_INFO'] = path
       env['QUERY_STRING'] = query_string
       env['REQUEST_METHOD'] = 'GET'
       env['REQUEST_URI'] = "https://#{req.host}#{path}"
-      #TODO
-      #use update(hash) { |name, old_value, new_value| }
+      # TODO use update(hash) { |name, old_value, new_value| }
       
-      #call the rack stack
+      # call the rack stack
       status, headers, body = @app.call(env)
       
       if status == 200
-        
-        puts headers['Content-Type']
-        puts body.inspect
         
         case headers['Content-Type'].split(';').first
           when 'text/html', 'application/json'          
@@ -112,7 +129,7 @@ class TelegramBotMiddleware
               elsif query.include?(:video)
                 send_to_bot('sendVideo', query)              
               else
-                #todo: show invalid
+                # TODO: invalid query
               end
               
             else
@@ -132,15 +149,16 @@ class TelegramBotMiddleware
         end
       end
       
-      #return result
+      # return result
       [status, headers, body]
     else
-      #normal rack flow - not a bot call
+      # normal rack flow - not a bot call
       @app.call(env)
     end
   end
   
   def send_to_bot(path, query)
     response = self.class.post("/bot#{@config.token}/#{path}", query: query)
+    # TODO check respobse error
   end
 end
